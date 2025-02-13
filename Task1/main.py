2b80a15677881cf4198fd2853f758af028fafca9
stomp 4 3 <150149808+b4suicid3@users.noreply.github.com>
2025-02-13 09:11:19 +0300

    Update main.py

--- Task1/main.py
+++ Task1/main.py
@@ -1,4 +1,4 @@
-def main(x):
+def main(x: float) -> float:
     try:
         return (((pow((29 * pow(x, 3)) + 13 + x, 4) + ((70 * x) / 45))
                  / pow(x, 7)) + pow((51 * pow(x, 2)) + 1 + (88 * x), 3))
