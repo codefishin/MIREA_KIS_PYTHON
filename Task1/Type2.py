c182e85fe0cb96a6697b177afa5b8b017c1f0a08
stomp 4 3 <150149808+b4suicid3@users.noreply.github.com>
2025-02-13 10:19:50 +0300

    Update Type2.py

--- Task1/Type2.py
+++ Task1/Type2.py
@@ -1,6 +1,6 @@
 def main(x: float) -> float:
-    try:
-        return (((pow((29 * pow(x, 3)) + 13 + x, 4) + ((70 * x) / 45))
-                 / pow(x, 7)) + pow((51 * pow(x, 2)) + 1 + (88 * x), 3))
-    except ArithmeticError:
-        return 0
+    numerator = ((29 * x ** 3 + 13 + x) ** 4) + (70 * x) / 45
+    denominator = x ** 7
+    term1 = numerator / denominator
+    term2 = (51 * x ** 2 + 1 + 88 * x) ** 3
+    return term1 + term2
